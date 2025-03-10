3934f1fc60d73d6adf820ae9cdb786e1391d1881
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2025-03-10 11:43:10 -0300

    Add files via upload

---
+++ ms-produto_aula_06/src/main/java/br/com/fiap/ms_produto/controller/handlers/dto/ValidationErrorDTO.java
@@ -0,0 +1,28 @@
+package br.com.fiap.ms_produto.controller.handlers.dto;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationErrorDTO extends CustomErrorDTO {
+
+    private List<FielMessageDTO> errors = new ArrayList<>();
+
+
+
+    public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path){
+        super(timestamp, status, error, path);
+
+    }
+    //metodo para add erros à list
+    public void addError(String fieldName, String message){
+        //remove o erro de campos duplicados
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
+        errors.add(new FielMessageDTO(fieldName, message));
+    }
+
+
+}
---
+++ ms-produto_aula_06/target/README.md
@@ -0,0 +1,2 @@
+Nome: Rafael 
+RM: 98845
